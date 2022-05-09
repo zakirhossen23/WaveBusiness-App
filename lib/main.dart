df36251a9bfbcb6dfcf8361f7ab31c8ae963dbd8
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2022-05-10 04:51:29 +0600

    Wave business

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WaveData',
+      title: 'Wave Business',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
